13b581c974dc59fd6ef745c4d889c70b1cba890c
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-10-12 13:31:52 +0800

    beta 3
    a lot of work done to get the template working.
    copy-files-module created a lot of problem.

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,47 @@
+import type { DefineComponent } from 'vue'
+// the alternative to Nuxt theme https://nuxt-themes.netlify.app/
+// removed bloginote in the root and move all other to the root and add site as parent obj
 export default defineAppConfig({
   myLayer: {
-    name: 'Hello from Nuxt layer'
-  }
+    name: 'Hello from Nuxt layer',
+    menuBtn1: 'Google'
+  },
+  copyFiles: {
+    content: 'wrong/content',
+    public: 'wrong/public'
+  },
+  meta: {
+    url: 'https://create-google-app.vercel.app/'
+  },
+  site: {
+    author: 'Kyrie Eleison',
+    email: 'jur.eleison@gmail.com',
+    avatar: '/avatar.svg',
+    favicon: '/avatar.svg', // favicon.ico
+    scrollSmooth: true,
+  },
+  homePage: {
+    showBlogPosts: true,
+    postItemLimit: 20
+  },
+  privacyPage: true,
+  articlePage: {
+    showTime: true,
+    outdated: {
+      show: true,
+      threshold: 30
+    },
+    showCatalog: false,
+  },
+  search: {
+    exclude: [
+      '/',
+      '/about/',
+      '/privacy/',
+      '/list/'
+    ]
+  },
+  assetTypes: []
 })
 
 declare module '@nuxt/schema' {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,98 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import pkg from './package.json'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+// import { createResolver } from '@nuxt/kit'
+// const { resolve } = createResolver(import.meta.url)
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+// https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases
+
+//grepper capitalize first letter in all words in a string, separeted with space ' ' or hyphen '-' (like name in package.json)
+const capitalize = (string) => {
+  const words = string.split(/[\s-]+/)
+  for (let i in words) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1)
+  }
+  return words.join(' ')
+}
+// console.log('The pkg.name is now changed to: ' + capitalize(pkg.name))
+//end grepper
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: { /* baseURL: '/' */ },
+  css: [
+    join(currentDir, './assets/style.css')
+  ],
+  modules: [
+    // [resolve('./modules/copy-files-module'), { cleanFolders: ['public/article'] }],
+    '@nuxt/content',
+    '@nuxtjs/tailwindcss',
+    '@vite-pwa/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-svgo'
+  ],
+  nitro: {
+    prerender: {
+      routes: ['/rss.xml', '/sitemap.xml']
+    }
+  },
+  // https://content.nuxtjs.org
+  content: {
+    navigation: {
+      fields: ['_id', '_type', 'series', 'tags']
+    },
+    highlight: {
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
+      theme: 'one-dark-pro',
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md for the available program language
+      preload: ['vue', 'python', 'tsx']
+    },
+    markdown: {
+      toc: {
+        depth: 5,
+        searchDepth: 5
+      },
+      remarkPlugins: ['remark-math', 'remark-sub', 'remark-super'],
+      rehypePlugins: {
+        'rehype-katex': {
+          // https://katex.org/docs/options.html#:~:text=output
+          output: 'mathml' // the default value is `htmlAndMathml`
+        }
+      }
+    },
+    ignores: [/* ['images', 'attachment'] */],
+  },
+  typescript: {
+    shim: false,
+    strict: false,
+    typeCheck: true
+  },
+  runtimeConfig: {
+    timestamp: {
+      created: false,
+      updated: false
+    },
+    site: { // renamed from rss
+      title: capitalize(pkg.name),
+      description: 'Guide to Create Google App in one day or one week, depending on the programming skills.',
+      image: `${pkg.homepage}/avatar.svg`, // avatar also in appconfig
+      favicon: `${pkg.homepage}/avatar.svg`, // favicon also in appconfig
+      copyright: `All rights reserved ${(new Date()).getFullYear()}, Kyrie Eleison`
+    },
+    public: {
+      hostname: pkg.homepage,
+    }
+  },
+  components: [
+    { path: './components/custom', pathPrefix: false  },
+    { path: './components/content', pathPrefix: false  },
+    { path: './components'}
+    // https://nuxt.com/docs/guide/directory-structure/components
+  ],
+  svgo: {
+    // https://nuxt.com/docs/guide/going-further/layers#relative-paths-and-aliases
+    autoImportPath: join(currentDir, '/assets/icons/'),
+    // svgoConfig: {}
+  },
 })
