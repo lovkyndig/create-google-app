b35ec357b8d212f45ddcdcb7d05ea7bbe9d74f48
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-10-24 12:37:04 +0800

    v1.0.0-rc.61
    precaching google-verification-file (test1)

---
+++ server/routes/google7f989f055a7284bd.html.ts
@@ -0,0 +1,7 @@
+import { defineEventHandler } from 'h3'
+export default defineEventHandler(async (event) => {
+  // @ts-ignore
+  await appendHeader(event, 'Content-Type', 'text/html')
+
+  return 'google-site-verification: google7f989f055a7284bd.html'
+})
