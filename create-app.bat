c8e26882162678b95c45f088c44908bfe5309660
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-10-31 16:49:56 +0800

    v1.0.5-rc.5

--- create-app.bat
+++ create-app.bat
@@ -5,6 +5,11 @@ clear
 @echo (1) Make sure you have updated the "manifest.webmanifest" with all the new info about your app.
 @echo (2) Make sure you have updated the "pwa"-folders in "public"-folder, with your own icons & images.
 @echo ---
+@echo REMEMBER WHEN THIS SCRIPT IS FINISHED TO WRITE THIS COMMANDs:
+@echo :
+@echo cd bubblewrap
+@echo bubblewrap build
+@echo ---
 @setlocal enableextensions enabledelayedexpansion
 @echo off
 echo - What is the name of this folder (the repo-name)?
