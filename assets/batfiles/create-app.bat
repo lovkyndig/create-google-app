0872537bc3a78533fc7b5bd7b93777378ea8d168
lovkyndig <125480548+lovkyndig@users.noreply.github.com>
2023-10-31 14:49:37 +0800

    v1.0.1-rc.4
    uploading this version as v1 to G Play

---
+++ assets/batfiles/create-app.bat
@@ -0,0 +1,16 @@
+clear
+@cls
+@echo THIS BAT-FILE HELP YOU WITH CREATING THE BUBBLEWRAP-BUNDLE (THE APP)
+@echo ---
+@echo (1) Make sure you have updated the "manifest.webmanifest" with all the new info about your app.
+@echo (2) Make sure you have updated the "pwa"-folders in "public"-folder, with your own icons & images.
+@echo ---
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+echo - What is the name of this folder (the repo-name)?
+set /p input= Ex: my-repo-name: 
+set variable=%input%
+:: echo !variable!
+md bubblewrap
+bubblewrap init --manifest="https://%input%.vercel.app/manifest.webmanifest" --directory="bubblewrap"
+endlocal
